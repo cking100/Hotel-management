3d04eb13408f4ab22b6ada079690876afcfa1519
chirag <c.taneja09@gmail.com>
2024-10-10 23:08:21 +0530

    Custom Exception

---
+++ Hotel_management/src/main/java/com/chirag/Hotel_management/exception/OurException.java
@@ -0,0 +1,8 @@
+package com.chirag.Hotel_management.exception;
+
+public class OurException extends RuntimeException {
+
+    public OurException(String message) {
+        super(message);
+    }
+}
