55b8a1f90655e1a89958943d323d2cd952e69c5a
chirag <c.taneja09@gmail.com>
2024-10-19 21:48:55 +0530

    Authenticate controller

---
+++ Hotel_management/src/main/java/com/chirag/Hotel_management/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.chirag.Hotel_management.controller;
+
+import com.chirag.Hotel_management.dto.LoginRequest;
+import com.chirag.Hotel_management.dto.Response;
+import com.chirag.Hotel_management.entity.User;
+import com.chirag.Hotel_management.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody User user) {
+        Response response = userService.register(user);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response> login(@RequestBody LoginRequest loginRequest) {
+        Response response = userService.login(loginRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+}
